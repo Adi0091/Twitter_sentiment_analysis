15b297608bcd2c2cf267646ab7b18ae048a6417a
Aditya Vijay Waghaye <153804438+Adi0091@users.noreply.github.com>
2025-11-20 18:16:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,30 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from transformers import DistilBertTokenizerFast, TFDistilBertForSequenceClassification
+import gdown
+import zipfile
 
+MODEL_ZIP_URL = "https://drive.google.com/uc?export=download&id=1UKN34y0z36gLn2B52U8XXRxcwdNrDf1q"
 MODEL_DIR = "artifacts/transformer_model"
+ZIP_PATH = "transformer_model.zip"
+
+def download_and_extract_model():
+    if not os.path.exists(MODEL_DIR):
+        os.makedirs(MODEL_DIR, exist_ok=True)
+
+    if os.path.exists(os.path.join(MODEL_DIR, "config.json")):
+        return
+
+    gdown.download(MODEL_ZIP_URL, ZIP_PATH, quiet=False)
+    
+    with zipfile.ZipFile(ZIP_PATH, "r") as zip_ref:
+        zip_ref.extractall(MODEL_DIR)
+
+    os.remove(ZIP_PATH)
+
+download_and_extract_model()
+
+# MODEL_DIR = "artifacts/transformer_model"
 
 
 tokenizer = DistilBertTokenizerFast.from_pretrained(MODEL_DIR)
@@ -29,3 +51,4 @@ if st.button("Predict"):
     label = "Positive" if pred == 1 else "Negative"
 
     st.success(f"Prediction: {label}")
+
